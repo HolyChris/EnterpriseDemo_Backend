b91c976535c0678add5451b0404b613946b594b6
Priyank Gupta <priyank.gupta@vinsol.com>
2015-01-13 21:04:51 +0530

    assign temp password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,16 @@ class User < ActiveRecord::Base
   has_many :sites, through: :site_managers
 
   after_save :touch_auth_token, if: "encrypted_password_changed? || sign_in_count_changed?"
+  before_create :assign_temp_password
 
   # def fullname
   #   [firstname, lastname].join(' ')
   # end
+
+  def assign_temp_password
+    self.password ||= '12345678'
+    self.password_confirmation ||= '12345678'
+  end
 
   def auth_token_expired?
     auth_token_created_at? && (auth_token_created_at + AUTH_TOKEN_EXPIRATION) < Time.current
