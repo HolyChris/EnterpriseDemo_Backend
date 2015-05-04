255859a38a3ee4dba6e266a532c32f1cbfa4b3fb
Priyank Gupta <priyank.gupta@vinsol.com>
2015-05-05 01:21:37 +0530

    minor changes

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,8 +7,8 @@ set :rails_env, 'staging'                  # If the environment differs from the
 set :branch, 'master'
 
 # set :unicorn_pid, shared_path.join("tmp/pids/unicorn.pid")
-# set :unicorn_config, shared_path.join("config/unicorn.rb")
-set :unicorn_workers, 4
+set :unicorn_config, shared_path.join("config/unicorn.rb")
+# set :unicorn_workers, 4
 # set :unicorn_service,
 
 set :nginx_server_name, '54.68.73.69'
