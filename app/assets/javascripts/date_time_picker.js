075913d7dfc79e514fc70b3e3286eac94597d2ff
Priyank Gupta <priyank.gupta@vinsol.com>
2015-05-07 23:03:38 +0530

    minor fix

--- app/assets/javascripts/date_time_picker.js
+++ app/assets/javascripts/date_time_picker.js
@@ -1,6 +1,15 @@
-$(document).on('ready page:change', function(){
+$(document).on('page:change', function(){
   $(".datetimepicker").each(function(){
     $(this).datetimepicker({
+      step: 60,
+      format: 'M d Y, h:i a',
+      formatDate: 'M d Y',
+      formatTime: 'h:i a'
+    });
+  });
+
+  $('.has_many_container.follow_ups').on('has_many_add:after', function(e, fieldset, container){
+    $(fieldset).find('.datetimepicker').datetimepicker({
       step: 60,
       format: 'M d Y, h:i a',
       formatDate: 'M d Y',
