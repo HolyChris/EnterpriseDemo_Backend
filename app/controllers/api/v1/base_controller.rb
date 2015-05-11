c68cec24b9551421bc7f49f74bce680ca57ca816
Priyank Gupta <priyank.gupta@vinsol.com>
2015-05-11 15:28:22 +0530

    remove csrf protection from api

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::BaseController < ActionController::Base
   respond_to :json
 
-  # skip_before_action :verify_authenticity_token
-  # before_action :authenticate_user_from_token!
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user_from_token!
    
   private
 
-    def verify_authenticity_token
+    def authenticate_user_from_token!
       unless current_user
-        render json: { message: 'User not authorized to perform the operation' }, status: 422
+        render json: { message: 'User not authorized to perform the operation' }, status: 404
       end
     end
 
