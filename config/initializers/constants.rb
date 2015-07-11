22924665b63d2ad3dbc6a18010038b62583d3282
Vipin Nagpal <vipin.itm@gmail.com>
2015-07-11 15:53:49 +0530

    Add constants for heroku config

--- config/initializers/constants.rb
+++
@@ -1,3 +0,0 @@
-EMAIL_REGEXP = %r{\A(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}\Z}xi
-AUTH_TOKEN_EXPIRATION = 3.weeks
-PER_PAGE = 10
