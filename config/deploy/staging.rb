2e1246fe98fc730771dc5109b60a237aadc1516d
Manoj <manoj.delhi24@gmail.com>
2015-08-01 01:34:31 +0530

    branch corrected for staging deplyment script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 set :rails_env, 'staging'                  # If the environment differs from the stage name
-set :branch, 'customer-portal-changes'
+set :branch, 'development'
 
 # set :unicorn_pid, shared_path.join("tmp/pids/unicorn.pid")
 set :unicorn_config, shared_path.join("config/unicorn.rb")
