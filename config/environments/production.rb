c1b8762d1a9aab1dfa19cf3aa813f57c050b9854
Manoj <manoj.delhi24@gmail.com>
2015-08-20 00:57:12 +0530

    emails list updated for error notificaions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,5 +100,11 @@ Rails.application.config.middleware.use ExceptionNotification::Rack,
   :email => {
     :email_prefix => "ERS: Exception on production ",
     :sender_address => %{"notifier" <notifier@ecoroofs.com>},
-    :exception_recipients => %w{vipin.itm@gmail.com production@doublebitconsulting.com chris@doublebitconsulting.com}
+    :exception_recipients => %w{vipin.itm@gmail.com
+                                production@doublebitconsulting.com
+                                chris@doublebitconsulting.com
+                                sehrawat.manoj24@gmail.com
+                                jeffshamley@gmail.com
+                                rodolfo.leyes@toptal.com
+                              }
   }
