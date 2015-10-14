73591b2b043daacd97a6e8450bdd97c21887adde
Manoj <manoj.delhi24@gmail.com>
2015-10-15 02:59:29 +0530

    primary manager assigment for a site

--- app/models/site_manager.rb
+++ app/models/site_manager.rb
@@ -3,4 +3,11 @@ class SiteManager < ActiveRecord::Base
   belongs_to :user
 
   validates :site, :user, presence: true
+
+  before_save :manage_primary, if: [:primary?, :primary_changed?]
+
+  private
+    def manage_primary
+      site.site_managers.where.not(id: id).update_all(primary: false)
+    end
 end
