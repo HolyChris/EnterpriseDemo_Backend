8b4b74d3a58266b60c53fb2c551b167e075895d3
Marcin Skłodowski <marcinsklodowski@gmail.com>
2016-01-22 11:25:23 +0100

    Changing JsonKeyFormat to camelized.

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -1,6 +1,6 @@
 JSONAPI.configure do |config|
   #:underscored_key, :camelized_key, :dasherized_key, or custom
-  config.json_key_format = :dasherized_key
+  config.json_key_format = :camelized_key
 
   #:underscored_route, :camelized_route, :dasherized_route, or custom
   config.route_format = :dasherized_route
