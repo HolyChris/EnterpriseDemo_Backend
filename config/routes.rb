4a897da21997378b94967ec7d8b56a52784ad8c3
Marcin Skłodowski <marcinsklodowski@gmail.com>
2016-01-25 11:03:12 +0100

    Adjustor portal route

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,8 @@ Rails.application.routes.draw do
       namespace :customer_portal do
         resource :customer, only: [:show]
       end
-      namespace :adjustor_portal do
-        resource :adjustor, only: [:show]
-      end
 
+      get 'adjustor_portal', to: 'adjustor_portal/adjustors#show'
 
       resources :home, only: :index
       resources :users, only: :index do
