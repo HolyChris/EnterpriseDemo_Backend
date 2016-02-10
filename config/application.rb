bd12db000367b2fa5fc00ee275631c682e2f7682
Erik Mingo <erikmingo@Eriks-MacBook-Air.local>
2016-02-09 16:21:05 -0800

    precompile?

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,10 @@ module Ers
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(active_admin.js.coffee)
 
     config.middleware.insert_before 0, "Rack::Cors" do
+
       allow do
         origins '*'
         resource '/api/v1/*',
