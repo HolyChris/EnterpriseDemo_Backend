62fe04509c46011e905a59fcb1f30aabefd267f3
Jesse Litton <jesse.litton@gmail.com>
2016-02-09 22:32:01 -0800

    Adds new photos for modals. limits to nine for loading purposes.

--- app/assets/javascripts/modal_toggle.js
+++ app/assets/javascripts/modal_toggle.js
@@ -1,11 +1,11 @@
-$(function() {
-  $(".overlay-container").click(function(event) {
+$(document).ready(function() {
+  $(".thumb-1").click(function(event) {
     event.preventDefault();
-    console.log('clicked');
-    var $modalContainer = $(this).closest('.portalModals');
-    var $action = $modalContainer.find('.bg-overlay');
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-1');
 
     if ($action.hasClass("closed")) {
+    console.log('clicked');
       $action
         .removeClass("closed")
           .addClass("open");
@@ -20,7 +20,247 @@ $(function() {
     event.preventDefault();
 
     var $modalContainer = $(this).closest('.portalModals');
-    var $action = $modalContainer.find('.bg-overlay');
+    var $action = $modalContainer.find('.ph-1');
+
+    $action
+      .removeClass("open")
+        .addClass("closed");
+  });
+
+  //
+
+  $(".thumb-2").click(function(event) {
+    event.preventDefault();
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-2');
+
+    if ($action.hasClass("closed")) {
+    console.log('clicked');
+      $action
+        .removeClass("closed")
+          .addClass("open");
+    } else if ($action.hasClass("open")) {
+      $action
+        .removeClass("open")
+          .addClass("closed");
+    }
+  });
+
+  $(".close-modal").click(function(event) {
+    event.preventDefault();
+
+    var $modalContainer = $(this).closest('.portalModals');
+    var $action = $modalContainer.find('.ph-2');
+
+    $action
+      .removeClass("open")
+        .addClass("closed");
+  });
+
+  //
+
+  $(".thumb-3").click(function(event) {
+    event.preventDefault();
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-3');
+
+    if ($action.hasClass("closed")) {
+    console.log('clicked');
+      $action
+        .removeClass("closed")
+          .addClass("open");
+    } else if ($action.hasClass("open")) {
+      $action
+        .removeClass("open")
+          .addClass("closed");
+    }
+  });
+
+  $(".close-modal").click(function(event) {
+    event.preventDefault();
+
+    var $modalContainer = $(this).closest('.portalModals');
+    var $action = $modalContainer.find('.ph-3');
+
+    $action
+      .removeClass("open")
+        .addClass("closed");
+  });
+
+  //
+
+  $(".thumb-4").click(function(event) {
+    event.preventDefault();
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-4');
+
+    if ($action.hasClass("closed")) {
+    console.log('clicked');
+      $action
+        .removeClass("closed")
+          .addClass("open");
+    } else if ($action.hasClass("open")) {
+      $action
+        .removeClass("open")
+          .addClass("closed");
+    }
+  });
+
+  $(".close-modal").click(function(event) {
+    event.preventDefault();
+
+    var $modalContainer = $(this).closest('.portalModals');
+    var $action = $modalContainer.find('.ph-4');
+
+    $action
+      .removeClass("open")
+        .addClass("closed");
+  });
+
+  //
+
+  $(".thumb-5").click(function(event) {
+    event.preventDefault();
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-5');
+
+    if ($action.hasClass("closed")) {
+    console.log('clicked');
+      $action
+        .removeClass("closed")
+          .addClass("open");
+    } else if ($action.hasClass("open")) {
+      $action
+        .removeClass("open")
+          .addClass("closed");
+    }
+  });
+
+  $(".close-modal").click(function(event) {
+    event.preventDefault();
+
+    var $modalContainer = $(this).closest('.portalModals');
+    var $action = $modalContainer.find('.ph-5');
+
+    $action
+      .removeClass("open")
+        .addClass("closed");
+  });
+
+  //
+
+  $(".thumb-6").click(function(event) {
+    event.preventDefault();
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-6');
+
+    if ($action.hasClass("closed")) {
+    console.log('clicked');
+      $action
+        .removeClass("closed")
+          .addClass("open");
+    } else if ($action.hasClass("open")) {
+      $action
+        .removeClass("open")
+          .addClass("closed");
+    }
+  });
+
+  $(".close-modal").click(function(event) {
+    event.preventDefault();
+
+    var $modalContainer = $(this).closest('.portalModals');
+    var $action = $modalContainer.find('.ph-6');
+
+    $action
+      .removeClass("open")
+        .addClass("closed");
+  });
+
+  //
+
+  $(".thumb-7").click(function(event) {
+    event.preventDefault();
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-7');
+
+    if ($action.hasClass("closed")) {
+    console.log('clicked');
+      $action
+        .removeClass("closed")
+          .addClass("open");
+    } else if ($action.hasClass("open")) {
+      $action
+        .removeClass("open")
+          .addClass("closed");
+    }
+  });
+
+  $(".close-modal").click(function(event) {
+    event.preventDefault();
+
+    var $modalContainer = $(this).closest('.portalModals');
+    var $action = $modalContainer.find('.ph-7');
+
+    $action
+      .removeClass("open")
+        .addClass("closed");
+  });
+
+  //
+
+  $(".thumb-8").click(function(event) {
+    event.preventDefault();
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-8');
+
+    if ($action.hasClass("closed")) {
+    console.log('clicked');
+      $action
+        .removeClass("closed")
+          .addClass("open");
+    } else if ($action.hasClass("open")) {
+      $action
+        .removeClass("open")
+          .addClass("closed");
+    }
+  });
+
+  $(".close-modal").click(function(event) {
+    event.preventDefault();
+
+    var $modalContainer = $(this).closest('.portalModals');
+    var $action = $modalContainer.find('.ph-8');
+
+    $action
+      .removeClass("open")
+        .addClass("closed");
+  });
+
+  //
+
+  $(".thumb-9").click(function(event) {
+    event.preventDefault();
+    var $modalContainer = $(this).closest('.portal-container');
+    var $action = $modalContainer.find('.ph-9');
+
+    if ($action.hasClass("closed")) {
+    console.log('clicked');
+      $action
+        .removeClass("closed")
+          .addClass("open");
+    } else if ($action.hasClass("open")) {
+      $action
+        .removeClass("open")
+          .addClass("closed");
+    }
+  });
+
+  $(".close-modal").click(function(event) {
+    event.preventDefault();
+
+    var $modalContainer = $(this).closest('.portalModals');
+    var $action = $modalContainer.find('.ph-9');
 
     $action
       .removeClass("open")
